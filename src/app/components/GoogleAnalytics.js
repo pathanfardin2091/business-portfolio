5ccc0b022107e92e5501a3b2d8b6762e2cff2d90
Fardin Samsher Pathan <fardinpathan2091@gmail.com>
2026-01-02 16:22:30 +0530

    chore: clean install + next security fix

---
+++ src/app/components/GoogleAnalytics.js
@@ -0,0 +1,23 @@
+"use client";
+
+import Script from "next/script";
+
+export default function GoogleAnalytics({ GA_MEASUREMENT_ID }) {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+
+      <Script strategy="afterInteractive" id="ga-script">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
@@ -31,19 +32,8 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
 
-        {/* <Script
-  src={`https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX`}
-  strategy="afterInteractive"
-/>
+        <GoogleAnalytics GA_MEASUREMENT_ID="G-77QF7MFC72" />
 
-<Script id="ga-setup" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-XXXXXXXXXX');
-  `}
-</Script> */}
 
         {children}
 
